01f6c04131fd009e8aa0a80c67463569c2cc8e9d
hieunguyen26 <nguyencaohieu26@gmail.com>
2021-07-11 19:41:33 +0700

    blogchild

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -76,3 +76,30 @@
  $(".stop").on("click", function () {
    owl.trigger("stop.owl.autoplay");
  });
+//  slider for relate blog
+var owl = $(".related-post__list");
+owl.owlCarousel({
+  items: 3,
+  loop: true,
+  margin: 20,
+  autoplay: false,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    500: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+$(".play").on("click", function () {
+  owl.trigger("play.owl.autoplay", [1000]);
+});
+$(".stop").on("click", function () {
+  owl.trigger("stop.owl.autoplay");
+});
